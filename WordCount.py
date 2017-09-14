61b7c8e5f0d831b80584aa72a0ae8ad2b74b87d8
Toyhoshi <toyhoshi@users.noreply.github.com>
2017-09-14 15:24:11 +0200

    Update WordCount.py

--- WordCount.py
+++ WordCount.py
@@ -2,15 +2,10 @@
 from operator import add
 from pyspark import SparkContext
 
-
-if __name__ == "__main__":
-    sc = SparkContext(appName="WordCount")
-    lines = sc.textFile("hdfs:///marco/README.txt")
-    counts = lines.flatMap(lambda x: x.split(' ')) \
-                  .map(lambda x: (x, 1)) \
-                  .reduceByKey(add)
-    output = counts.collect()
-    for (word, count) in output:
-        print str(word) +": "+ str(count);
-
-    sc.stop()
+sc = SparkContext(appName="WordCount")
+lines = sc.textFile("hdfs:///marco/README.txt")
+counts = lines.flatMap(lambda x: x.split(' ')).map(lambda x: (x, 1)).reduceByKey(add)
+output = counts.collect()
+for (word, count) in output:
+    print str(word) +": "+ str(count);
+sc.stop()
